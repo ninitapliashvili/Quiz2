161bf83c5fc5b78729a49701f7006dae17f2339a
ninitapliashvili <tafliashvilinini@gmail.com>
2023-04-09 18:28:34 +0400

    tasks 3 and 4

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -77,3 +77,64 @@ Console.WriteLine(isEcual);
  * 3.   დაწერეთ პროგრამა რომელიც მასივში დაბეჭდავს ერთმანეთის მეზობლად მდგარი ტოლი ელემენტების მაქსიმალურ რაოდენობას და ამ ელემენტს. 
  *      Მაგ 1 1 2 2 2 3 4 2 2  - უნდა დაბეჭდოს 3  და 2.  (3)
  */
+
+int maxNum = 0, tempElement = 0, count = 1, element = 0;
+
+void maxNumOfE(int[] nums)
+{
+    tempElement = nums[0];
+    for(int i = 0; i < nums.Length;i++)
+    {
+        if (nums[i] == tempElement)
+        {
+            count++;
+        }
+        else
+        {
+            if (count > maxNum)
+            {
+                maxNum = count;
+                element = tempElement;
+            }
+            count = 1;
+            tempElement = nums[i];
+        }
+    }
+    Console.WriteLine($"Max num of {element}: {maxNum}");
+}
+
+int[] arr = { 1, 1, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 2, 2, };
+
+maxNumOfE(arr);
+
+/*
+ * 4.   (ბონუსი) დაწერეთ პროგრამა რომელიც მასივში იპოვნის ყველაზე ხშირად გამეორებული ელემენტის რაოდენობას და დაბეჭდავს ამ ელემენტს. (2)
+ */
+
+int e = 0, temp = 0, tempcounter = 1, counter = 0;
+
+int repeatedElement(int[] nums)
+{
+    temp = nums[0];
+    for (int i = 0; i < nums.Length; i++)
+    {
+        for (int j = 0; j < nums.Length; j++)
+        {
+            if (nums[j] == temp)
+            {
+                tempcounter++;
+            }
+        }
+        if(tempcounter > counter)
+        {
+            counter = tempcounter;
+            e = temp;
+        }
+
+        tempcounter = 1;
+        temp = nums[i];
+        
+    }
+    return e;
+}
+Console.WriteLine($"A frequently repeated element is {repeatedElement(arr)}");
